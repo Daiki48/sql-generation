471d64b7a49732a42ca87ccbdeed1e8101967604
Daiki48 <daiki48.engineer@gmail.com>
2024-11-07 23:09:13 +0900

    :zap: Added sql-generation info command

--- src/cli_info.rs
+++ src/cli_info.rs
@@ -1,3 +1,19 @@
-pub fn info() {
-    println!("This is info command");
+use std::fs;
+use std::path::PathBuf;
+use std::error::Error;
+use toml;
+
+pub fn info(home_dir: PathBuf) -> Result<(), Box<dyn Error>> {
+    let config_path = home_dir.join(".sql-generation").join("config.toml");
+    if !config_path.exists() {
+        return Err("config.toml file does not exist. Run `sql-generation init` command first.".into());
+    }
+
+    let config_content = fs::read_to_string(config_path)?;
+    let config: toml::Value = toml::from_str(&config_content)?;
+
+    println!("Configuration:");
+    println!("{}", toml::to_string_pretty(&config)?);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,19 @@ struct Args {
 
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
+    #[command(about = "Initialization")]
     Init,
+    
+    #[command(about = "Refers to config.toml")]
     Info,
+
+    #[command(about = "Edit configuration values in config.toml")]
     Config,
+
+    #[command(about = "Add SQL template in config.toml")]
     Add,
+
+    #[command(about = "Remove SQL template in config.toml")]
     Remove,
 }
 
@@ -35,7 +44,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match args.cmd {
         Commands::Init => cli_init::init(home_dir, consts::INIT_CONFIG)?,
-        Commands::Info => cli_info::info(),
+        Commands::Info => cli_info::info(home_dir)?,
         Commands::Config => cli_config::config(),
         Commands::Add => cli_add::add(),
         Commands::Remove => cli_remove::remove(),
